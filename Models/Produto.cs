697a91d45f2751f64758ab8024cc736925e11bfd
kelsontabachini <kelsontabachini@gmail.com>
2023-03-14 22:28:09 -0300

    kelsontabachini

---
+++ Models/Produto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BSIStore.Models
+{
+    public class Produto
+    {
+        public int Id {get; set;}
+        public string Descricao {get; set;}
+        [DataType(DataType.Date)]public DateTime validade{get; set;}
+        public string categoria {get; set;}
+        public double preco {get; set;}
+
+    }           
+        /*
+            Id
+            Descricao
+            Validade
+            Categoria
+            Preco
+        */
+}    
